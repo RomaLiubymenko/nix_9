7a7f53e761e1a958bc2a108b74ba3d6eeab98d8e
RomaLiubymenko <poznan449@gmail.com>
2021-10-15 14:22:24 +0300

    added correct description of tasks

--- hw_01_base_operations/src/main/java/ua/com/alevel/Main.java
+++ hw_01_base_operations/src/main/java/ua/com/alevel/Main.java
@@ -13,10 +13,21 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
+        String commonMessage = "<html><p>Enter a string in the input field and click on \"Get result\".</p><br>";
+        String firstTaskDescription = "<p>As a result, you should get the sum of all the numbers in the line.</p><br>" +
+                "<p>If no numbers are found in the string - \"Empty result\"</p><br>";
+        String secondTaskDescription = "<p>As a result, you should get sorted characters with the number of occurrences " +
+                "<br>of each character in the string.</p><br>" +
+                "<p>If no letters are found in the string - \"Empty result\"</p><br>";
+        String thirdTaskDescription = "<html><p>In some school, classes start at 9:00.<br>" +
+                "<p>The duration of the lesson is 45 minutes, after the 1st, 3rd, 5th, etc. lessons,<br>" +
+                "a break of 5 minutes, and after the 2nd, 4th, 6th, etc. - 15 minutes.<br>" +
+                "Enter a number of lesson from 1-10 in the input field and click on \"Get result\".<br>" +
+                "As a result, you will get the end time of this lesson</p><br>";
         Map<Task, TaskService> tasks = Map.of(
-                new Task(0, "First task", "Some description for First task"), new FirstTaskServiceImpl(),
-                new Task(1, "Second task", "Some description for Second task"), new SecondTaskServiceImpl(),
-                new Task(2, "Third task", "Some description for Third task"), new ThirdTaskServiceImpl()
+                new Task(0, "First task", commonMessage + firstTaskDescription), new FirstTaskServiceImpl(),
+                new Task(1, "Second task", commonMessage + secondTaskDescription), new SecondTaskServiceImpl(),
+                new Task(2, "Third task", thirdTaskDescription), new ThirdTaskServiceImpl()
         );
         EventQueue.invokeLater(() -> new MainScreen(tasks).setVisible(true));
     }
