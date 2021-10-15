30d26eccc1f3a6279452ff9323d2d818eb93e10b
RomaLiubymenko <poznan449@gmail.com>
2021-10-15 12:28:24 +0300

    added solution for third task

--- hw_01_base_operations/src/main/java/ua/com/alevel/service/implementation/ThirdTaskServiceImpl.java
+++ hw_01_base_operations/src/main/java/ua/com/alevel/service/implementation/ThirdTaskServiceImpl.java
@@ -2,10 +2,23 @@ package ua.com.alevel.service.implementation;
 
 import ua.com.alevel.service.TaskService;
 
-public class ThirdTaskServiceImpl  implements TaskService {
+import java.util.regex.Pattern;
+
+public class ThirdTaskServiceImpl implements TaskService {
 
     @Override
     public String getSolutionForTaskByString(String stringToSearch) {
-        return stringToSearch + "Third";
+        String regexFromOneToTen = "[1-9]|10";
+        if (Pattern.matches(regexFromOneToTen, stringToSearch)) {
+            int numberOfLesson = Integer.parseInt(stringToSearch);
+            int numberOfRecess = numberOfLesson - 1;
+            int timeOfRecess = (numberOfRecess / 2 * 20) + (numberOfRecess % 2) * 5;
+            int timeOfLesson = numberOfLesson * 45;
+            int allTimeInMinutes = timeOfRecess + timeOfLesson;
+            int hours = allTimeInMinutes / 60;
+            int minutes = allTimeInMinutes % 60;
+            return hours + 9 + " " + minutes;
+        }
+        return "Empty result";
     }
 }
