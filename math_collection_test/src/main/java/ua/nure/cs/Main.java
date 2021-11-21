3c22efe805d6e2f2d551fdfeaee1ca0503969434
RomaLiubymenko <poznan449@gmail.com>
2021-11-21 22:09:42 +0200

    added showSortFrame

--- math_collection_test/src/main/java/ua/nure/cs/Main.java
+++ math_collection_test/src/main/java/ua/nure/cs/Main.java
@@ -1,10 +1,13 @@
 package ua.nure.cs;
 
+import ua.nure.cs.sort.SortFrame;
 import ua.nure.cs.util.ArrayUtils;
 
 public class Main {
 
     public static void main(String[] args) {
+        SortFrame sortFrame = new SortFrame("Array Sorting Testing");
+        sortFrame.showSortFrame();
         ArrayUtils.testSearchMethods();
     }
 }
