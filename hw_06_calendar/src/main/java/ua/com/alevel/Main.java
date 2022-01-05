2046a5a5a066d078b0bc837d13b81c8c44cadc5c
RomaLiubymenko <poznan449@gmail.com>
2022-01-06 00:04:02 +0200

    clean main in hw_06

--- hw_06_calendar/src/main/java/ua/com/alevel/Main.java
+++ hw_06_calendar/src/main/java/ua/com/alevel/Main.java
@@ -1,24 +1,8 @@
 package ua.com.alevel;
 
-import ua.com.alevel.date.Calendar;
-
 public class Main {
 
     public static void main(String[] args) {
-        Calendar calendar = new Calendar(100,11,1,5,10,11,2021);
-//        Calendar calendar = new Calendar(63826549271100L);
-        long millis = calendar.getTimeInMillis();
-
-        System.out.println("calendar.getTimeInMillis() = " + millis);
-
-        System.out.println(calendar.getMillisFromMillis(millis));
-        System.out.println(calendar.getSecondFromMillis(millis));
-        System.out.println(calendar.getMinuteFromMillis(millis));
-        System.out.println(calendar.getHourFromMillis(millis));
-        System.out.println(calendar.getDayFromMillis(millis));
-        System.out.println(calendar.getMonthFromMillis(millis));
-        System.out.println(calendar.getYearFromMillis(millis));
-        System.out.println("___-------___-------___");
     }
 
 }
